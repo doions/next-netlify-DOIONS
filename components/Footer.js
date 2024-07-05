ef609a952bd338d331697268f8d3151f14addff5
Doions <43123876+doions@users.noreply.github.com>
2024-07-05 22:04:56 +0530

    Update Footer.js example

--- components/Footer.js
+++ components/Footer.js
@@ -1,22 +1,65 @@
 import styles from './Footer.module.css'
 
-export default function Footer() {
+
+import { useState } from 'react';
+
+
+export default function Home() {
+  const encode = (data) => {
+    return Object.keys(data)
+      .map(
+        (key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key])
+      )
+      .join('&');
+  };
+
+  const handleSubmit = (e) => {
+    const data = {
+      name: e.target.name.value,
+      email: e.target.email.value,
+      message: e.target.message.value,
+    };
+
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({ 'form-name': 'contact-form', ...data }),
+    })
+      .then(() => alert('Success!'))
+      .catch((error) => alert(error));
+
+    e.preventDefault();
+  };
+
+  const handleChange = (e) =>
+    this.setState({ [e.target.name]: e.target.value });
+
   return (
     <>
-      <footer className={styles.footer}>
-    <form name="contact" mothod="POST" netlify>
-    <input type="hidden" name="form-name" value="contact" />
-  <p>
-    <label>Name <input type="text" name="name" /></label>
-  </p>
-  <p>
-    <label>Email <input type="email" name="email" /></label>
-  </p>
-  <p>
-    <button type="submit">Send</button>
-  </p>
-</form>
-      </footer>
+      <main>
+        <p>Forms Example</p>
+        <form name='contact-form' method='post' onSubmit={handleSubmit}>
+          <input type='hidden' name='form-name' value='contact' />
+          <p>
+            <label>
+              Your Name: <input type='text' name='name' />
+            </label>
+          </p>
+          <p>
+            <label>
+              Your Email: <input type='email' name='email' />
+            </label>
+          </p>
+          <p>
+            <label>
+              Message: <textarea name='message'></textarea>
+            </label>
+          </p>
+          <p>
+            <button type='submit'>Send</button>
+          </p>
+        </form>
+      </main>
     </>
-  )
+  );
 }
