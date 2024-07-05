927fd99ed5e5218aac11ae7d7036219f904026dd
Doions <43123876+doions@users.noreply.github.com>
2024-07-05 21:11:37 +0530

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -4,6 +4,17 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
+    <form name="contact" netlify>
+  <p>
+    <label>Name <input type="text" name="name" /></label>
+  </p>
+  <p>
+    <label>Email <input type="email" name="email" /></label>
+  </p>
+  <p>
+    <button type="submit">Send</button>
+  </p>
+</form>
       </footer>
     </>
   )
