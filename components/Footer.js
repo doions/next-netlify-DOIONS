8a852c51d7d92f54c4750aa211e1b559ed254946
Doions <43123876+doions@users.noreply.github.com>
2024-07-09 12:40:13 +0530

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -37,28 +37,7 @@ export default function Home() {
   return (
     <>
       <main>
-        <p>Forms Example</p>
-        <form name='contact-form' method='post' onSubmit={handleSubmit}>
-          <input type='hidden' name='form-name' value='contact' />
-          <p>
-            <label>
-              Your Name: <input type='text' name='name' />
-            </label>
-          </p>
-          <p>
-            <label>
-              Your Email: <input type='email' name='email' />
-            </label>
-          </p>
-          <p>
-            <label>
-              Message: <textarea name='message'></textarea>
-            </label>
-          </p>
-          <p>
-            <button type='submit'>Send</button>
-          </p>
-        </form>
+       Contact: 
       </main>
     </>
   );
