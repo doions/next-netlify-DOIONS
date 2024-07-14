68cc28dc863f9941e04375059c8b3ad9a28e36b4
Doions <43123876+doions@users.noreply.github.com>
2024-07-14 09:03:45 +0530

    navigatin

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,16 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+const ulStyle = {
+  padding: 0,
+  listStyle: 'none',
+};
+
+const liStyle = {
+  float: 'left',
+  display: 'inline',
+  padding: '0 20px',
+};
 
 export default function Home() {
   return (
@@ -9,7 +19,16 @@ export default function Home() {
         <title>DOIONS - An AI Automation Company</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+      <div>
+      <nav>
+                        <ul style={ulStyle}>
+                            <li style={liStyle}><a style={aStyle} href="#">Home</a></li>
+                            <li style={liStyle}><a style={aStyle} href="#">About Us</a></li>
+                            <li style={liStyle}><a style={aStyle} href="#">Services</a></li>
+                            <li style={liStyle}><a style={aStyle} href="#">Contact Us</a></li>
+                        </ul>
+                    </nav>
+                    </div>
       <main>
        <img src="/logo.png" alt="DOIONS Logo"/>
       </main>
